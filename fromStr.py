e610442f6747cd33e5ac26b604db3be0e1a42ef3
lancekra123 <kramerlance14@gmail.com>
2024-08-29 23:22:22 -0800

    new update
    excited for this new update

--- fromStr.py
+++ fromStr.py
@@ -11,19 +11,19 @@ class large_num:
             "Dsg", "Tsg", "Qdsg", "Qnsg", "Sxsg", "Spsg", "Ocsg", "Nosg", "Sg", "USg", "DSg", "TSg", 
             "QdSg", "QnSg", "SxSg", "SpSg", "OcSg", "NoSg", "Og", "UOg", "DOg", "TOg", "QdOg", "QnOg", 
             "SxOg", "SpOg", "OcOg", "NoOg", "Ng", "UNg", "DNg", "QdNg", "QnNg", "SxNg", "SpNg", 
-            "OcNg", "NoNg", "Ce", "UCe", "DCe"
+            "OcNg", "NoNg", "Ce", "UCe", "DCe", "TCe"
         ]
 
         mag = 0
-        while Check.meeq(abs(num), 1000) and mag < len(suffixes) - 1:
+        while Check.meeq(abs(num), 1000.0) and mag < len(suffixes) - 1:
             mag = MathService.add(mag, 1)
             num = MathService.div(num, 1000.0)
         
-        if Check.eq(num, int(num)):
-            format = f"{int(num)}{suffixes[mag]}"
-        elif Check.le(num, 10):
-            format = f"{num:.2f}{suffixes[mag]}"
+        if Check.le(abs(num), 0.1):
+            formatted = f"{num:.2e}"
+        elif Check.le(abs(num), 10):
+            formatted = f"{num:.2f}"
         else:
-            format = f"{num:.1f}{suffixes[mag]}"
+            formatted = f"{num:.1f}"
 
-        return format
+        return f"{formatted}{suffixes[mag]}"
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from discord import app_commands
 from operation import MathService
 from fromStr import large_num
 
-NumberFormat = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+intent = discord.Intents.all()
+intent.message_content = True
+NumberFormat = commands.Bot(command_prefix="/", intents=intent)
 
 @NumberFormat.event
 async def on_ready():
@@ -38,6 +40,197 @@ async def add(interaction: discord.Interaction, value1: str, value2: str):
     embed.add_field(name="Result", value=result, inline=False)
     embed.add_field(name="To Short", value=toStr, inline=False)
     
+    await interaction.response.send_message(embed=embed)
+
+@NumberFormat.tree.command(name="sub", description="Sub 2 values")
+@app_commands.describe(value1="Number", value2="Number")
+async def sub(interaction: discord.Interaction, value1: str, value2: str):
+    try:
+        num1 = MathService.to_float(value1)
+        num2 = MathService.to_float(value2)
+    except ValueError:
+        await interaction.response.send_message("Invalid input. Please provide valid numbers.")
+        return
+
+    from_larg = large_num()
+    result = MathService.sub(num1, num2)
+    toStr = from_larg.toStr(result)
+    
+    new_title = "Result from sub value1 - value2"
+    embed = discord.Embed(
+        title=new_title,
+        description=f"{value1} - {value2} = {result}",
+        color=discord.Color.red()
+    )
+    embed.add_field(name="Result", value=result, inline=False)
+    embed.add_field(name="To Short", value=toStr, inline=False)
+    
+    await interaction.response.send_message(embed=embed)
+
+@NumberFormat.tree.command(name="div", description="Add 2 values")
+@app_commands.describe(value1="Number", value2="Number")
+async def div(interaction: discord.Interaction, value1: str, value2: str):
+    try:
+        num1 = MathService.to_float(value1)
+        num2 = MathService.to_float(value2)
+    except ValueError:
+        await interaction.response.send_message("Invalid input. Please provide valid numbers.")
+        return
+
+    from_larg = large_num()
+    result = MathService.div(num1, num2)
+    toStr = from_larg.toStr(result)
+    
+    new_title = "Result from dividing value1 / value2"
+    embed = discord.Embed(
+        title=new_title,
+        description=f"{value1} / {value2} = {result}",
+        color=discord.Color.red()
+    )
+    embed.add_field(name="Result", value=result, inline=False)
+    embed.add_field(name="To Short", value=toStr, inline=False)
+    
+    await interaction.response.send_message(embed=embed)
+
+@NumberFormat.tree.command(name="mul", description="mul 2 values")
+@app_commands.describe(value1="Number", value2="Number")
+async def mul(interaction: discord.Interaction, value1: str, value2: str):
+    try:
+        num1 = MathService.to_float(value1)
+        num2 = MathService.to_float(value2)
+    except ValueError:
+        await interaction.response.send_message("Invalid input. Please provide valid numbers.")
+        return
+
+    from_larg = large_num()
+    result = MathService.mul(num1, num2)
+    toStr = from_larg.toStr(result)
+    
+    new_title = "Result from multiply value1 * value2"
+    embed = discord.Embed(
+        title=new_title,
+        description=f"{value1} * {value2} = {result}",
+        color=discord.Color.red()
+    )
+    embed.add_field(name="Result", value=result, inline=False)
+    embed.add_field(name="To Short", value=toStr, inline=False)
+    
+    await interaction.response.send_message(embed=embed)
+
+@NumberFormat.tree.command(name="log", description="log 2 values")
+@app_commands.describe(value1="Number", value2="Number")
+async def log(interaction: discord.Interaction, value1: str, value2: str):
+    try:
+        num1 = MathService.to_float(value1)
+        num2 = MathService.to_float(value2)
+    except ValueError:
+        await interaction.response.send_message("Invalid input. Please provide valid numbers.")
+        return
+
+    from_larg = large_num()
+    result = MathService.log(num1, num2)
+    toStr = from_larg.toStr(result)
+    
+    new_title = f"Result from log(value1, value2)"
+    embed = discord.Embed(
+        title=new_title,
+        description=f"log({num1}, {num2}) = {result}",
+        color=discord.Color.red()
+    )
+    embed.add_field(name="Result", value=result, inline=False)
+    embed.add_field(name="To Short", value=toStr, inline=False)
+    
+    await interaction.response.send_message(embed=embed)
+
+@NumberFormat.tree.command(name="short", description="for Example 1000 to 1k")
+@app_commands.describe(value1="Number")
+async def short(interaction: discord.Interaction, value1: str):
+    try:
+        num1 = MathService.to_float(value1)
+    except ValueError:
+        await interaction.response.send_message("Invalid input. Please provide valid numbers.")
+        return
+
+    from_larg = large_num()
+    toStr = from_larg.toStr(num1)
+    
+    new_title = f"Short"
+    embed = discord.Embed(
+        title=new_title,
+        description=f"{toStr}",
+        color=discord.Color.red()
+        )
+    embed.add_field(name="Original Value", value=num1, inline=False)
+    
+    await interaction.response.send_message(embed=embed)
+
+    
+@NumberFormat.tree.command(name="pow", description="Power of ex. 10^3")
+@app_commands.describe(value1="Number", value2="Number")
+async def short(interaction: discord.Interaction, value1: str, value2: str):
+    try:
+        num1 = MathService.to_float(value1)
+        num2 = MathService.to_float(value2)
+    except ValueError:
+        await interaction.response.send_message("Invalid input. Please provide valid numbers.")
+        return
+
+    from_larg = large_num()
+    result = MathService.pow(num1, num2)
+    toStr = from_larg.toStr(result)
+    
+    new_title = f"Pow"
+    embed = discord.Embed(
+        title=new_title,
+        description=f"{toStr}",
+        color=discord.Color.red()
+        )
+    embed.add_field(name="Original Value", value=f"{num1}^{num2}={result}", inline=False)
+    
+    await interaction.response.send_message(embed=embed)
+
+@NumberFormat.tree.command(name="help", description="To Help figure out where i put my dinner at")
+async def help(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title="Help find the commands",
+        description="must use `/` before using a command",
+        color=discord.Color.orange()
+    )
+    embed.add_field(
+        name="add",
+        value="Add 2 Values",
+        inline=True
+    )
+    embed.add_field(
+        name="div",
+        value="Div 2 values",
+        inline=True
+    )
+    embed.add_field(
+        name="log",
+        value="log(value1, value2)",
+        inline=True
+    )
+    embed.add_field(
+        name="mul",
+        value="Mul 2 values",
+        inline=True
+    )
+    embed.add_field(
+        name="short",
+        value="short ex. 1000 to 1k",
+        inline=True
+    )
+    embed.add_field(
+        name="sub",
+        value="Sub 2 values",
+        inline=True
+    )
+    embed.add_field(
+        name="pow",
+        value="Pow value1^value2",
+        inline=True
+    )
     await interaction.response.send_message(embed=embed)
 
 NumberFormat.run()
